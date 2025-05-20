aac662474a5e331347f8f55171f5297cc4ef4b2c
analuizaluiza <analuizacardosouni@gmail.com>
2025-05-20 12:31:39 -0300

    corigindo a tabela users no userSchema

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,6 +1,6 @@
 const userSchema = `
   CREATE TABLE IF NOT EXISTS users (
-      userId VARCHAR(255) UNIQUE NOT NULL,
+      userId INT AUTO_INCREMENT PRIMARY KEY,
       email VARCHAR(255) NOT NULL,
       password VARCHAR(255) NOT NULL,
       name VARCHAR(255) NOT NULL
